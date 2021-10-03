afc30c273d1826da1ffa20f9edae460de0908862
khoaphan1161996 <khoa071996@gmail.com>
2021-10-03 19:42:40 +0700

    finishProject1

---
+++ Project1/script.js
@@ -0,0 +1,10 @@
+// Toggle Menu
+let navLinks = document.querySelector('#nav-links');
+function showMenu() {
+    navLinks.style.right = '0'
+}
+
+function hideMenu() {
+    navLinks.style.right = '-150px'
+}
+
